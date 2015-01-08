bc674e3b3df848682f3207e331c5432686b7fccb
Kwag De Gi <code@skyaperture.com>
2015-01-08 10:36:24 +0900

    loop test

--- 0108/0108.py
+++ 0108/0108.py
@@ -5,3 +5,20 @@ word2 = "morning"
 
 sentence = word1 + " " + word2
 print sentence
+
+
+a = 0
+
+while a < 10:
+    a = a + 1
+    print a
+
+
+z = 4
+
+if z > 70:
+    print "Something is very strange"
+elif z < 7:
+    print "Normal"
+
+
