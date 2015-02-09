49b1d268a930f3525c3dcc8f1c5a22e59e85c9b9
Kwag De Gi <code@skyaperture.com>
2015-02-10 08:51:47 +0900

    Come on over

---
+++ 0210/come.py
@@ -0,0 +1,10 @@
+  
+class Come_on_over:
+    def __init__(self, come, on, over):
+        print come, on, over
+
+come = 'come'
+on = 'on'
+over = 'over'
+        
+a = Come_on_over(come, on, over)
