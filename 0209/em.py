48400a25611f218607519aef48d5cc616df45bc2
Kwag De Gi <code@skyaperture.com>
2015-02-09 13:39:29 +0900

    class

---
+++ 0209/em.py
@@ -0,0 +1,19 @@
+
+class Employee:
+    'common base class for all employees'
+    empCount = 0
+
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+        Employee.empCount += 1
+
+    def displayCount(self):
+        print "Total Employee %d" % Employee.empCount
+
+    def displayEmployee(self):
+        print "Name : ", self.name, ", Salary: ", self.salary
+
+emp1 = Employee("Zara", 2000)
+
+print emp1.displayEmployee()
