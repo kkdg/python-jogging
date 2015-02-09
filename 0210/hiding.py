df3066fdea1ae82e9106cc2feee851adb1335f4c
Kwag De Gi <code@skyaperture.com>
2015-02-10 08:28:33 +0900

    hiding member var

---
+++ 0210/hiding.py
@@ -0,0 +1,13 @@
+
+class JustCounter:
+    __secretCount = 0
+
+    def count(self):
+        self.__secretCount += 1
+        print self.__secretCount
+
+counter = JustCounter()
+counter.count()
+counter.count()
+
+print counter.__secretCount
