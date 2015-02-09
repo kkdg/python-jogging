98407b965705cb4870e508af864b9eff1fb2aa96
Kwag De Gi <code@skyaperture.com>
2015-02-10 08:16:51 +0900

    inheritance

---
+++ 0210/py.py
@@ -0,0 +1,23 @@
+class Parent:
+	parentAttr = 100
+        def __init__(self):
+            print "Calling parent constructor"
+
+        def parentMethod(self):
+            print "calling parent method"
+
+        def setAttr(self, attr):
+            Parent.parentAttr = attr
+
+        def getAttr(self):
+            print "Parent Attribute : ", Parent.parentAttr
+
+class Child(Parent):
+        def __init__(self):
+            print "Calling child constructor"
+
+        def childMethod(self):
+            print "Calling child method"
+
+c = Child()
+c.childMethod()
