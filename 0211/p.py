de08faa96fefd380d88d339188e0ecff90f8a754
Kwag De Gi <code@skyaperture.com>
2015-02-11 08:06:16 +0900

    prime number

--- 0211/p.py
+++ 0211/p.py
@@ -2,3 +2,14 @@ a = ['Mary', 'had', 'a', 'little', 'lamb']
 
 for i in range(len(a)):
     print i, a[i]
+
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print n, 'equals', x, '*', n/x
+            break
+
+    else:
+        print n, 'is a prime number'
+
+
