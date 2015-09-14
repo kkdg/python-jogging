34554d46bcfcbd6f1015df97ddb1c74e77d5b7a7
Kwag De Gi <code@skyaperture.com>
2015-09-14 17:09:53 +0900

    NPV and IRR

--- 0914/lib.py
+++ 0914/lib.py
@@ -1,7 +1,26 @@
 # Present Value of Future Cash Flow
 
-def pv_f(fv, r, n):
+def get_pv(fv, r, n):
+    ### fv : future value
+    ### r : interest rate
+    ### n : number of periods
     return fv/(1+r)**n
 
-def fv_p(pv, r, n):
+def get_fv(pv, r, n):
+    ### pv : present value
+    ### r : interest rate
+    ### n : number of periods
     return pv*(1+r)**n
+
+def get_npv(rate, cashflows):
+    total = 0.0
+    for i, cashflow in enumerate(cashflows):
+        total += cashflow / (1+rate)**i
+    return total
+
+def get_irr(cashflows, interations=100):
+    rate = 1.0
+    investment = cashflows[0]
+    for i in range(1, interations+1):
+        rate*= (1-get_npv(rate,cashflows)/investment)
+    return rate
